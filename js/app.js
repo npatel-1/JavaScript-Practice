0a4047fd3b6820cb6b5aea3c28bcd47d808f84ed
Tech-A03 <tech-a03@Tech-A03s-MacBook-Air.local>
2018-09-15 17:04:25 +0100

    completed tutorial

--- js/app.js
+++ js/app.js
@@ -45,3 +45,200 @@
 // colors.push('purple');
 //    this will display purple because we added purple and it is number 3 in the array
 // alert(colors[3]);
+ // var numbers = [5,77,6,'seven',15,55];
+//    length displays the amount of values in an array
+ // alert(numbers.length);
+ //   a method as 2 brackets after it
+ //   sort will arrange the values in an array in order
+ // alert(numbers.sort());
+ //   revers will display the values in an array from end to beginning
+ // alert(numbers.reverse());
+//    LOOPS
+//    in the console, i will start from 0 and will add 1(i++) untill it reaches 9
+//    for loop
+// for(var i = 0; i < 10; i++){
+//    this will display it to 10 because we made i less or equal to 10
+// for(var i = 0; i <= 10; i++){
+//   console.log('I love the number '+i);
+// }
+//    while loop
+//displays 0 to 9
+// var i = 0;
+// while (i < 10) {
+//   console.log(i);
+//   i++;
+// }
+//    forEach loop
+//displays each value in an array on a new line
+// var numbers = [33,54,76,34,2,6];
+// numbers.forEach(function(number){
+//   console.log(number);
+// });
+//using for loops with an array will also display each value in a new line
+// var numbers = [33,54,76,34,2,6];
+// reverse will display each value on a new line but from end values till start
+// numbers.reverse();
+// for(var i = 0;i < numbers.length;i++){
+//   console.log(numbers[i]);
+// }
+
+//    CONDITIONALS
+// 1 equals is aissigning 1 to a variable or element for example
+// 2 equals is comparing
+// 3 equals comparing but the 2 data sets have to match
+// this is not the same because one is a integer and the other is string
+// if(1 == '1'){
+// the console will display this is true as they both match
+// if(1 == 1){
+//   console.log('this is true');
+// }
+// this will give nothing in the console
+// if(1 == 2){
+//   console.log('this is true');
+// }
+// var var1 = 3;
+// var var2 = 4;
+// ! is to test if its not true (it flips the value)
+// if(var1 != var2){
+//   console.log('this is true');
+// }
+// as the values do not match, it will ignore the if statement and read the else statement to show that it is false
+// var var1 = 3;
+// var var2 = 4;
+// && means AND, so var1 == var2 has to be true along with var1 == 3
+// var var1 = 3;
+// var var2 = 4;
+
+// if(var1 == var2 && var1 == 3){
+//   console.log('this is true');
+// } else console.log ('this is false')
+// || means OR
+// if(var1 == var2 || var1 == 3){
+//   console.log('this is true');
+// } else console.log ('this is false')
+// as grapes is not listed, it will read out the default
+// var fruit = 'grapes';
+//
+// switch(fruit){
+//   case "banana":
+//     alert('I hate bananas')
+//     break;
+//   case "apple":
+//     alert('I love apples');
+//     break;
+//   case "orange":
+//     alert('Oranges is OK');
+//     break;
+//   default:
+//     alert('I love all other fruits');
+//     break;
+// }
+//    OBJECTS
+// Object literal
+//objects use {}
+// var person = {
+//     firstName: 'Neil',
+//     lastName: 'Patel',
+//     age: 21,
+//     children: ['Brianna', 'Nicholas'],
+//     address: {
+//       street: '555 Something at',
+//       city: 'London',
+//       country: 'England'
+//     },
+//     fullName: function(){
+      // this referes to the current object it is in. So for instance it will be person
+//       return this.firstName + " "+this.lastName;
+//     }
+// }
+//
+// console.log(person.fullName());
+
+//Object Constructor
+
+// var apple = new Object();
+// apple.color = 'red';
+// apple.shape = 'round';
+// apple.describe = function(){
+//   return 'An apple is the color '+this.color+' and is the shape '+this.shape;
+// }
+// describe is the function
+// console.log(apple.describe());
+
+// function Fruit(name, color, shape){
+//   this.name = name;
+//   this.color = color;
+//   this.shape = shape;
+//
+//   this.describe = function(){
+//     return 'A '+this.name+' is the color ' +this.color+ ' and is the shape'+this.shape;
+//   }
+//
+// }
+
+// var apple = new Fruit('apple', 'red', 'round');
+// var melon = new Fruit('melon', 'green', 'round');
+// this will display round in the console
+// console.log(melon.shape);
+// console.log(melon.describe());
+
+// var users = [
+//   {
+//     name: 'Neil Patel',
+//     age: 21
+//   },
+//   {
+//     name: 'Peter Parkar',
+//     age: 16
+//   },
+//   {
+//     name: 'Shelly Williams',
+//     age: 30
+//   }
+// ];
+// this will display the name and age of the first person.
+// console.log(users[0]);
+// this will make it more specific and only display the name only
+// console.log(users[0].name);
+
+//    EVENTS
+// this will changed the heading when the button is clicked.
+// function changeText(){
+//   var heading = document.getElementById('heading');
+//   heading.innerHTML = 'You Clicked!';
+// }
+// function showDate(){
+//   var time = document.getElementById('time');
+//   time.innerHTML = Date();
+// }
+//
+// function clearDate(){
+//   var time = document.getElementById('time');
+//   time.innerHTML = '';
+// }
+
+//    FORMS
+// function changeBackground(x){
+//   console.log(x.value);
+//   var body = document.getElementById('body');
+//   body.style.backgroundColor = x.value;
+// }
+// function changeBackground(x){
+//   console.log(x.value);
+//   var heading = document.getElementById('heading');
+//   heading.style.color = x.value;
+// }
+
+// function validateForm(){
+//   var firstName = document.forms["myForm"]["firstName"].value;
+// if the
+  // if(firstName == null || firstName == ""){
+  //   alert("First Name is required");
+  //   return false;
+  // }
+// if the characters is below 3 then an alert will pop up
+//   if(firstName.length < 3){
+//     alert("First Name must be at least 3 characters");
+//     return false;
+//   }
+// }
